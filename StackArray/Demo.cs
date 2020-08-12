5c08b7c5b1fde95dddefc2198f201688028e314a
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-12 21:21:49 +0530

    Add project files.

---
+++ StackArray/Demo.cs
@@ -0,0 +1,57 @@
+﻿using StackArray;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackArrayProject
+{
+    class Demo
+    {
+        static void Main(String[] args)
+        {
+            int choice, x;
+
+            StackA st = new StackA(8);
+
+            while(true)
+            {
+                Console.WriteLine("1.Push an element to the Stack");
+                Console.WriteLine("2.Pop an element from the Stack");
+                Console.WriteLine("3.Display the top element");
+                Console.WriteLine("4.Display all elements");
+                Console.WriteLine("5.Display the size of the stack");
+                Console.WriteLine("6.Quit");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 6)
+                    break;
+
+                switch(choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter an element to be inserted:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        st.Push(x);
+                        break;
+                    case 2:
+                        x = st.Pop();
+                        Console.WriteLine("Popped element is: "+x);
+                        break;
+                    case 3:
+                        Console.WriteLine("The top element is: " + st.Peek());
+                        break;
+                    case 4:
+                        st.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("The size of the stack is:" + st.Size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ StackArray/StackA.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StackArray
+{
+    class StackA
+    {
+        private int[] StackArray;
+        private int top;
+
+        public StackA()
+        {
+            StackArray = new int[10];
+            top = -1;
+        }
+
+        public StackA(int maxSize)
+        {
+            StackArray = new int[maxSize];
+            top = -1;
+        }
+
+        public int Size()
+        {
+            return top + 1;
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+        public bool isFull()
+        {
+            return (top == StackArray.Length - 1);
+        }
+
+        public void Push(int x)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Stack Overflow");
+            }
+            top = top + 1;
+            StackArray[top] = x;
+        }
+
+        public int Pop()
+        {
+            int x;
+            if (isEmpty())
+                throw new System.InvalidOperationException("Stack Underflow");
+            x = StackArray[top];
+            top = top - 1;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Stack Underflow");
+            return StackArray[top];
+        }
+
+        public void Display()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("Stack is:");
+            for(int i=top;i>=0;i--)
+            {
+                Console.WriteLine(StackArray[i]+ " ");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
